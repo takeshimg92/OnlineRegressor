2cfba21ba6cf792442fc2181d35ba76cc5defe39
Alessandro Morita <takeshi.gagliardi@gmail.com>
2023-10-20 10:23:47 -0300

    debugging folder

--- app.py
+++ app.py
@@ -78,10 +78,12 @@ if __name__ == '__main__':
         #st.expander("Como devem estar formatados meus dados?").write(msn.data_upload_explanation)
 
         #data = st.file_uploader("Adicionar base com covariáveis e alvo para treino (.xlsx)")
+        import os
+        st.write(os.getcwd())
         if problem_type == 'Classificação':
-            data = 'data/Módulo 3.2 - Bases processadas.xlsx'
+            data = './data/Módulo 3.2 - Bases processadas.xlsx'
         else:
-            data = 'data/Módulo 3.1 - Bases processadas.xlsx'
+            data = './data/Módulo 3.1 - Bases processadas.xlsx'
             
         X, y, run_training = get_features_and_target(data)
 
