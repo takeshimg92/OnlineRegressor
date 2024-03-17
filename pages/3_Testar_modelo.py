003e8a8c204543a7a6079d8739193d93bb1e58c4
Alessandro Morita <takeshi.gagliardi@gmail.com>
2024-03-17 17:39:56 -0300

    added download button

--- pages/3_Testar_modelo.py
+++ pages/3_Testar_modelo.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import pandas as pd
+import io
 
+def to_excel(df):
+        output = io.BytesIO()
+        with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+            df.to_excel(writer, index=False, sheet_name='Sheet1')
+            writer.close()
+        processed_data = output.getvalue()
+        return processed_data
+    
 if  st.session_state.get('is_fit') is None:
     st.write("Você precisa treinar um modelo antes")
     st.write("### Importante! \n As features devem ser *exatamente* as mesmas usadas para treino")
@@ -14,6 +23,11 @@ else:
             X_val = df_val[st.session_state['features']]
             df_pred = X_val.copy()
             df_pred['Prediction'] = model.predict_proba(X_val)[:,1]
-            st.write("É possível fazer download desta tabela clicando no símbolo no canto direito superior:")
+            st.download_button(
+                label="Baixar dados",
+                data=to_excel(df_pred),
+                file_name="base_scorada.xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
             st.dataframe(df_pred)
 
